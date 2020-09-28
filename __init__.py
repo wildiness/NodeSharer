658c97d02b8daebf95861d74d837af321cf627ee
Otto Kellerman <killerman_otto@hotmail.com>
2020-09-29 01:36:32 +0200

    add init

---
+++ __init__.py
@@ -0,0 +1,21 @@
+bl_info = {
+    "name": "Node Sharer",
+    "author": "NodeSharer Devs",
+    "version": (0, 1, 0),
+    "blender": (2, 90, 0),
+    "location": "Node Editor Toolbar",
+    "description": "Share node setups as text strings.",
+    "warning": "Did I name this beta? It's actually alpha AF",
+    "doc_url": "TBA",
+    "category": "Node",
+}
+
+
+def register():
+    from . import nodesharer
+    nodesharer.register()
+
+
+def unregister():
+    from . import nodesharer
+    nodesharer.unregister()
